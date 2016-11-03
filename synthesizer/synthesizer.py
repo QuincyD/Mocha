350af4c1c11d7ac6f75c3891892c1d361c341495
Joseph Korany <jkorany@gmail.com>
2016-11-03 16:53:03 -0400

    Refined additive noise a little bit

--- synthesizer/synthesizer.py
+++ synthesizer/synthesizer.py
@@ -24,12 +24,14 @@ class Synthesizer(threading.Thread):
 		self._queueIn = queueIn
 		self.frequency = (baseFrequency + maxDiffFrequency)/2.0
 		self.amplitude = 0.0
+		self.maxAmplitude = .75
 		self.phase = 0
 		self.fs = 44100
 		self.time = 0
 		self.baseFreq = baseFrequency
 		self.diffBaseMax = maxDiffFrequency
 		self.signal = None
+		self.lastSignal = [];
 		self.csvReader = None
 		self.pos = None
 		self.firstSignal = False
@@ -45,7 +47,7 @@ class Synthesizer(threading.Thread):
 					channels=1,
 					rate=self.fs,
 					output=True,
-					frames_per_buffer=4096/20,
+					frames_per_buffer=512,#4096/8,
 					stream_callback=callback)
 
 	def __enter__(self):
@@ -78,16 +80,22 @@ class Synthesizer(threading.Thread):
 	def updateSignal(self, frame_count, fadein=False, fadeout=False):
 		internal = 2*np.pi*self.frequency*(self.time + np.arange(frame_count)/float(self.fs)) + self.phase
 		signal = (np.sin(internal)).astype(np.float32)
-		offset = 1.
-		noise1 = (np.sin(internal - offset)).astype(np.float32)
-		noise2 = (np.sin(internal + offset)).astype(np.float32)
-		self.signal = .2*noise1 + .2*noise2 + .6*signal
+		noise1 = (np.sin(internal + .25 * np.pi)).astype(np.float32)
+		noise2 = (np.sin(internal + .5 * np.pi)).astype(np.float32)
+		# Play around with shift and amplitudes of noise.
+		# Original (shift, amp): noise1:(.25, .75), noise2:(.5, .1)
+		self.signal = .75*noise1 + .2*noise2 + signal
+		maxVal = np.amax(self.signal)
+		self.signal = self.signal / maxVal
+		# newMax = np.amax(self.signal)
+		# print maxVal, newMax
+		# sys.stdout.flush()
 		# print self.signal
 		# sys.stdout.flush()
 
 		if fadein:
-			self.signal = np.linspace(0.0, 1.0, frame_count).astype(np.float32) * self.signal
-			self.amplitude = 1.0
+			self.signal = np.linspace(0.0, self.maxAmplitude, frame_count).astype(np.float32) * self.signal
+			self.amplitude = self.maxAmplitude
 		elif fadeout:
 			self.signal = np.linspace(1.0, 0.0, frame_count).astype(np.float32) * self.amplitude * self.signal
 			self.amplitude = 0.0
@@ -105,7 +113,7 @@ class Synthesizer(threading.Thread):
 			with open('output.txt', 'rb') as f:
 				self.csvReader = list(csv.reader(f))
 			self.csvIndex = 0
-			self.amplitude = 1.0
+			self.amplitude = self.maxAmplitude
 
 		self.pos = self.csvReader[self.csvIndex]
 		self.csvIndex += 1
@@ -145,8 +153,12 @@ class Synthesizer(threading.Thread):
 
 		# Generate the new signal and return it to pyaudio
 		self.updateSignal(frame_count, self.firstSignal, fadeout)
+		# if len(self.lastSignal):
+		# 	print self.lastSignal[-1], self.signal[0]
+		# 	sys.stdout.flush()
 		# No longer the first signal in a new playback "session"
 		self.firstSignal = False
+		self.lastSignal = self.signal
 		return (self.signal, pyaudio.paContinue)
 
 	def setPos(self, pos):
