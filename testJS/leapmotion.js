27bac721d714aa9b94a33028d8ecdb30d3e98658
Brandon Mendrick <bmendric@umich.edu>
2016-11-04 15:07:41 -0400

    Added hand determiation and clicking

--- testJS/leapmotion.js
+++ testJS/leapmotion.js
@@ -16,12 +16,51 @@ function LeapMotion() {
       var frame = my_controller.frame();
       if (frame.valid) {
         interactionBox = frame.interactionBox;
-        hand = frame.hands[0];
+
+        //Determining which hand to use (prefers left)
+        switch(frame.hands.length) {
+          case 0:
+            hand = null;
+            break;
+
+          case 1:
+            hand = frame.hands[0];
+            break;
+
+          case 2:
+            if (frame.hands[0].type === "Left") {
+              hand = frame.hands[0];
+            } else {
+              hand = frame.hands[1];
+            }
+            break;
+
+          default:
+            hand = null;
+            break;
+        }
+
         if (hand && hand.palmPosition) {
           normalized = interactionBox.normalizePoint(hand.palmPosition);
-          if (normalized[0]) {
+
+          //Determining click
+          for (var i = 0; i < hand.pointables.length; i++) { //This check might be better to have in the UI update... need more info on how we are handling that though.
+            var pointable = hand.pointables[i]
+
+            if (pointable.type === 1) {
+              var dist = pointable.touchDistance
+
+              if (dist < 0) {
+                console.log("click")
+              }
+
+              break;
+            }
+          }
+
+          //Updating sound
+          if (normalized[0]) {    // Not sure if this is necessary. hand.palmPosition should not be true if it does not contain anything.
             freq = 200 + normalized[0] * 440;
-            // console.log(freq);
             synthesizer.updateFundFreq(freq);
             synthesizer.changeVolume(1 - normalized[1]);
           }
