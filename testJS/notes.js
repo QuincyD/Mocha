0812a0e1af470bd497af851d1843a0bb579264d5
QuincyD <quincyd@umich.edu>
2016-11-11 10:06:14 -0500

    Style update for notes.js

--- testJS/notes.js
+++ testJS/notes.js
@@ -1,222 +1,22 @@
-notes = [];
-frequencies = [];
+var notes = [];
+var frequencies = [];
 
-notes[0] = "C0";
-frequencies[0] = 16.35;
-notes[1] = "C#0/Db0";
-frequencies[1] = 17.32;
-notes[2] = "D0";
-frequencies[2] = 18.35;
-notes[3] = "D#0/Eb0";
-frequencies[3] = 19.45;
-notes[4] = "E0";
-frequencies[4] = 20.60;
-notes[5] = "F0";
-frequencies[5] = 21.83;
-notes[6] = "F#0/Gb0";
-frequencies[6] = 23.12;
-notes[7] = "G0";
-frequencies[7] = 24.50;
-notes[8] = "G#0/Ab0";
-frequencies[8] = 25.96;
-notes[9] = "A0";
-frequencies[9] = 27.50;
-notes[10] = "A#0/Bb0";
-frequencies[10] = 29.14;
-notes[11] = "B0";
-frequencies[11] = 30.87;
-notes[12] = "C1";
-frequencies[12] = 32.70;
-notes[13] = "C#1/Db1";
-frequencies[13] = 34.65;
-notes[14] = "D1";
-frequencies[14] = 36.71;
-notes[15] = "D#1/Eb1";
-frequencies[15] = 38.89;
-notes[16] = "E1";
-frequencies[16] = 41.20;
-notes[17] = "F1";
-frequencies[17] = 43.65;
-notes[18] = "F#1/Gb1";
-frequencies[18] = 46.25;
-notes[19] = "G1";
-frequencies[19] = 49.00;
-notes[20] = "G#1/Ab1";
-frequencies[20] = 51.91;
-notes[21] = "A1";
-frequencies[21] = 55.00;
-notes[22] = "A#1/Bb1";
-frequencies[22] = 58.27;
-notes[23] = "B1";
-frequencies[23] = 61.74;
-notes[24] = "C2";
-frequencies[24] = 65.41;
-notes[25] = "C#2/Db2";
-frequencies[25] = 69.30;
-notes[26] = "D2";
-frequencies[26] = 73.42;
-notes[27] = "D#2/Eb2";
-frequencies[27] = 77.78;
-notes[28] = "E2";
-frequencies[28] = 82.41;
-notes[29] = "F2";
-frequencies[29] = 87.31;
-notes[30] = "F#2/Gb2";
-frequencies[30] = 92.50;
-notes[31] = "G2";
-frequencies[31] = 98.00;
-notes[32] = "G#2/Ab2";
-frequencies[32] = 103.83;
-notes[33] = "A2";
-frequencies[33] = 110.00;
-notes[34] = "A#2/Bb2";
-frequencies[34] = 116.54;
-notes[35] = "B2";
-frequencies[35] = 123.47;
-notes[36] = "C3";
-frequencies[36] = 130.81;
-notes[37] = "C#3/Db3";
-frequencies[37] = 138.59;
-notes[38] = "D3";
-frequencies[38] = 146.83;
-notes[39] = "D#3/Eb3";
-frequencies[39] = 155.56;
-notes[40] = "E3";
-frequencies[40] = 164.81;
-notes[41] = "F3";
-frequencies[41] = 174.61;
-notes[42] = "F#3/Gb3";
-frequencies[42] = 185.00;
-notes[43] = "G3";
-frequencies[43] = 196.00;
-notes[44] = "G#3/Ab3";
-frequencies[44] = 207.65;
-notes[45] = "A3";
-frequencies[45] = 220.00;
-notes[46] = "A#3/Bb3";
-frequencies[46] = 233.08;
-notes[47] = "B3";
-frequencies[47] = 246.94;
-notes[48] = "C4";
-frequencies[48] = 261.63;
-notes[49] = "C#4/Db4";
-frequencies[49] = 277.18;
-notes[50] = "D4";
-frequencies[50] = 293.66;
-notes[51] = "D#4/Eb4";
-frequencies[51] = 311.13;
-notes[52] = "E4";
-frequencies[52] = 329.63;
-notes[53] = "F4";
-frequencies[53] = 349.23;
-notes[54] = "F#4/Gb4";
-frequencies[54] = 369.99;
-notes[55] = "G4";
-frequencies[55] = 392.00;
-notes[56] = "G#4/Ab4";
-frequencies[56] = 415.30;
-notes[57] = "A4";
-frequencies[57] = 440.00;
-notes[58] = "A#4/Bb4";
-frequencies[58] = 466.16;
-notes[59] = "B4";
-frequencies[59] = 493.88;
-notes[60] = "C5";
-frequencies[60] = 523.25;
-notes[61] = "C#5/Db5";
-frequencies[61] = 554.37;
-notes[62] = "D5";
-frequencies[62] = 587.33;
-notes[63] = "D#5/Eb5";
-frequencies[63] = 622.25;
-notes[64] = "E5";
-frequencies[64] = 659.25;
-notes[65] = "F5";
-frequencies[65] = 698.46;
-notes[66] = "F#5/Gb5";
-frequencies[66] = 739.99;
-notes[67] = "G5";
-frequencies[67] = 783.99;
-notes[68] = "G#5/Ab5";
-frequencies[68] = 830.61;
-notes[69] = "A5";
-frequencies[69] = 880.00;
-notes[70] = "A#5/Bb5";
-frequencies[70] = 932.33;
-notes[71] = "B5";
-frequencies[71] = 987.77;
-notes[72] = "C6";
-frequencies[72] = 1046.50;
-notes[73] = "C#6/Db6";
-frequencies[73] = 1108.73;
-notes[74] = "D6";
-frequencies[74] = 1174.66;
-notes[75] = "D#6/Eb6";
-frequencies[75] = 1244.51;
-notes[76] = "E6";
-frequencies[76] = 1318.51;
-notes[77] = "F6";
-frequencies[77] = 1396.91;
-notes[78] = "F#6/Gb6";
-frequencies[78] = 1479.98;
-notes[79] = "G6";
-frequencies[79] = 1567.98;
-notes[80] = "G#6/Ab6";
-frequencies[80] = 1661.22;
-notes[81] = "A6";
-frequencies[81] = 1760.00;
-notes[82] = "A#6/Bb6";
-frequencies[82] = 1864.66;
-notes[83] = "B6";
-frequencies[83] = 1975.53;
-notes[84] = "C7";
-frequencies[84] = 2093.00;
-notes[85] = "C#7/Db7";
-frequencies[85] = 2217.46;
-notes[86] = "D7";
-frequencies[86] = 2349.32;
-notes[87] = "D#7/Eb7";
-frequencies[87] = 2489.02;
-notes[88] = "E7";
-frequencies[88] = 2637.02;
-notes[89] = "F7";
-frequencies[89] = 2793.83;
-notes[90] = "F#7/Gb7";
-frequencies[90] = 2959.96;
-notes[91] = "G7";
-frequencies[91] = 3135.96;
-notes[92] = "G#7/Ab7";
-frequencies[92] = 3322.44;
-notes[93] = "A7";
-frequencies[93] = 3520.00;
-notes[94] = "A#7/Bb7";
-frequencies[94] = 3729.31;
-notes[95] = "B7";
-frequencies[95] = 3951.07;
-notes[96] = "C8";
-frequencies[96] = 4186.01;
-notes[97] = "C#8/Db8";
-frequencies[97] = 4434.92;
-notes[98] = "D8";
-frequencies[98] = 4698.63;
-notes[99] = "D#8/Eb8";
-frequencies[99] = 4978.03;
-notes[100] = "E8";
-frequencies[100] = 5274.04;
-notes[101] = "F8";
-frequencies[101] = 5587.65;
-notes[102] = "F#8/Gb8";
-frequencies[102] = 5919.91;
-notes[103] = "G8";
-frequencies[103] = 6271.93;
-notes[104] = "G#8/Ab8";
-frequencies[104] = 6644.88;
-notes[105] = "A8";
-frequencies[105] = 7040.00;
-notes[106] = "A#8/Bb8";
-frequencies[106] = 7458.62;
-notes[107] = "B8";
-frequencies[107] = 7902.13;
+var baseNotes = ["C", "C#/Db", "D", "D#/Eb", "E", "F",
+				 "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
+var count = 0;
+// Loop does not add the number to the sharp; 
+// Number is only added to the flat, but number is implied.
+for (i = 0; i < 9; i++){
+	for (j = 0; j < 12; j++){
+		notes[count] = baseNotes[j]+i.toString();
+		count++;
+	}
+}
+
+// formula for frequencies based on A4 (A4 = 440 Hz)
+for (i = -57,j = 0; i < 51; i++, j++) {
+	frequencies[j] = Math.round(440 * Math.pow(Math.pow(2, 1/12),i)*100)/100;
+}
 
 //Returns the closest note to the given frequency and the normalized distance
 function getNote(frequency) {
@@ -226,8 +26,9 @@ function getNote(frequency) {
 		return ['C0', (16.35 - frequency)/16.35];
 	}
 	if(frequency > 7902.13) {
-		return ['B8',frequency/7902.13];
 		console.log('Above highest frequency!');
+		return ['B8',(frequency - 7902.13)/7902.13];
+
 	}
 
 	lowerIndex = 0
@@ -237,7 +38,6 @@ function getNote(frequency) {
 		if (frequencies[i] > frequency) {
 			lowerIndex = i - 1;
 			upperIndex = i;
-			console.log('[',lowerIndex,',',upperIndex,']');
 			break;
 		}
 	}
